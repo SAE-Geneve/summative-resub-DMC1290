9126dfa1bfe6f6d987c9d01f50f7af0a3431a71d
DMC1290 <davideM.98@hotmail.com>
2024-12-11 13:48:49 +0100

    hello world

---
+++ summative_resub/summative_resub/game.cc
@@ -0,0 +1,11 @@
+#include "game.h"
+
+void Game::Struct()
+{
+
+}
+
+void Game::Loop()
+{
+
+}
---
+++ summative_resub/summative_resub/game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+
+
+class Game
+{
+public:
+
+	void Struct();
+
+	void Loop();
+
+};
+
+#endif // GAME_H
